26d4fa1f0c17217d33cd5ca0c4c9cec29216a032
Hiba-7 <jh_louzzani@esi.dz>
2025-05-02 22:21:19 +0200

    ajout des droit csp

--- index.js
+++ index.js
@@ -19,8 +19,19 @@ const io = socketIo(server, {
     methods: ["GET", "POST"],
   },
 });
-
+app.use((req, res, next) => {
+    res.setHeader(
+        "Content-Security-Policy",
+        "default-src 'self' https://accounts.google.com https://www.googleapis.com https://apis.google.com https://www.gstatic.com; " +
+        "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com https://www.googleapis.com https://apis.google.com https://www.gstatic.com; " +
+        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
+        "img-src 'self' data: https://www.gstatic.com; " +
+        "connect-src 'self' https://accounts.google.com https://www.googleapis.com https://apis.google.com https://www.gstatic.com;"
+    );
+    next();
+});
 app.use(cors()); 
+
 app.use(express.json());
 
 app.set("socketio", io); 
